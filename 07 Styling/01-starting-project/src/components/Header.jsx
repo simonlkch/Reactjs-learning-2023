a71921b8ae2a5c23efea333fd15ffdd46dceb0a5
mccreelkch <mccreelkch@gmail.com>
2024-01-08 15:45:03 +0800

    add: css module sample

--- 07 Styling/01-starting-project/src/components/Header.jsx
+++ 07 Styling/01-starting-project/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css'
+import classes from '../components/Header.module.css'
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.paragraph}>AAAAA community of artists and art-lovers.</p>
     </header>
   );
 }
