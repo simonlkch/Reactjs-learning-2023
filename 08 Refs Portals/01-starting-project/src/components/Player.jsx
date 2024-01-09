ba8c52433f359f879266b1a3c656c7bf37d4d8b1
mccreelkch <mccreelkch@gmail.com>
2024-01-09 12:26:24 +0800

    add ref sample

--- 08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ 08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState,useRef } from "react";
+
 export default function Player() {
+  const playerNameRef = useRef()
+  const [playerName,setPlayerName] = useState()
+
+  const handleSetName = () => {
+    setPlayerName(playerNameRef.current.value)
+    // just like get element by id usage
+    playerNameRef.current.value = ''
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName} entity</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerNameRef}/>
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   );
