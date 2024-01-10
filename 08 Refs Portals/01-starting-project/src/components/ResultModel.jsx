568ac32d7ded1599fbc3e46823ca64497a6559df
mccreelkch <mccreelkch@gmail.com>
2024-01-10 16:03:43 +0800

    add useImperativeHandle usage

--- 08 Refs Portals/01-starting-project/src/components/ResultModel.jsx
+++ 08 Refs Portals/01-starting-project/src/components/ResultModel.jsx
@@ -1,16 +1,25 @@
-import { forwardRef } from "react"
+import { useRef, forwardRef, useImperativeHandle } from "react"
 
 const ResultModel = forwardRef(function ({ result, targetTime }, ref) {
-    return (
-        <dialog ref={ref} className="result-modal">
-            <h2>You {result}</h2>
-            <p>The target is {targetTime} second(s)</p>
-            <p>You stop the timer with x second(s) left</p>
-            <form method="dialog">
-                <button>Close</button>
-            </form>
-        </dialog>
-    )
+  const dialog = useRef()
+  useImperativeHandle(ref, () => {
+    return{
+      show(){
+        dialog.current.showModal()
+      }
+    }
+  })
+
+  return (
+    <div ref={dialog} className="result-modal">
+      <h2>You {result}</h2>
+      <p>The target is {targetTime} second(s)</p>
+      <p>You stop the timer with x second(s) left</p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </div>
+  )
 })
 
 export default ResultModel
